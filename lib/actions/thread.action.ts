bbc6f04628c2eb350ca8480aa52ce41bef2ac623
Effham Ali <effhamali@192.168.10.11>
2024-03-02 00:58:25 +0500

    Thread logic in progress

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -57,9 +57,9 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     //   model: Community,
     // })
     .populate({
-      path: "children", 
+      path: "children",
       populate: {
-        path: "author", 
+        path: "author",
         model: User,
         select: "_id name parentId image",
       },
@@ -67,10 +67,82 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
 
   const totalPostsCount = await Thread.countDocuments({
     parentId: { $in: [null, undefined] },
-  }); 
+  });
   const posts = await postsQuery.exec();
 
   const isNext = totalPostsCount > skipAmount + posts.length;
 
   return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (e: any) {
+    throw new Error(`Error fetching thread: ${e.message}`);
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB();
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    const savedCommentThread = await commentThread.save();
+
+    originalThread.children.push(savedCommentThread._id);
+
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    throw new Error("Unable to add comment");
+  }
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread";
 
 interface Params {
   userId: string;
@@ -57,3 +58,27 @@ export async function fetchUser(userId: string) {
     throw new Error(`Failed to fetch user: ${error.message}`);
   }
 }
+
+export const fetchUserPosts = async (userId: string) => {
+  try {
+    connectToDB();
+
+    const threads = await User.findOne({ id: userId })
+      .populate({
+        path: "threads",
+        model: Thread,
+        populate: {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id",
+          },
+        },
+      })
+      .exec();
+
+    return threads;
+  } catch (error: any) {}
+};
